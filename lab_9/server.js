89b0eb00f1f1b646cc90cad05441412158b28985
Dwilson8 <45007566+Dwilson8@users.noreply.github.com>
2020-04-19 23:49:47 -0400

    Update server.js

--- lab_9/server.js
+++ lab_9/server.js
@@ -22,12 +22,12 @@ app.use(express.static('public'));
 
 
 function processDataForFrontEnd(req, res) {
-  const baseURL = ''; // Enter the URL for the data you would like to retrieve here
+  const baseURL = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json'; // Enter the URL for the data you would like to retrieve here
 
   // Your Fetch API call starts here
   // Note that at no point do you "return" anything from this function -
   // it instead handles returning data to your front end at line 34.
-    fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json')
+    fetch()
       .then((results) => results.json())
       .then((data) => { 
         console.log(data);
